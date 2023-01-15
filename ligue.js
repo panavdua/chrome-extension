f4b66392a7a184baf3804e0bb60e638aab913846
panavdua <panavdua@gmail.com>
2023-01-15 16:36:17 +0800

    Added rankings for different leagues

---
+++ ligue.js
@@ -0,0 +1,27 @@
+async function fetchLigue() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '7d8216ebb0msh38b7f2a7fe4e9f4p1e8b2ajsn7bc8bf122288',
+            'X-RapidAPI-Host': 'sports-live-scores.p.rapidapi.com'
+        }
+    };
+    
+    const res = await fetch('https://sports-live-scores.p.rapidapi.com/football/rankings/4', options)
+    const record = await res.json()
+    document.getElementById("liguerank").innerHTML = record.rankings.map(item => 
+        {
+            if (item.Rank < 5) {
+                return `<li id="blue">${item['Team Name']} - ${item.Points} Points \xa0 <i>(${item.Matches} matches)</i></li>`
+            } else if (item.Rank > 17) {
+                return `<li id="red">${item['Team Name']} - ${item.Points} Points \xa0 <i>(${item.Matches} matches)</i></li>`
+            } 
+            else {
+                return `<li>${item['Team Name']} - ${item.Points} Points \xa0 <i>(${item.Matches} matches)</i></li>`
+            }
+        }).join(' ');
+    
+    
+    console.log(record)
+}
+fetchLigue();
--- script.js
+++ script.js
@@ -10,22 +10,31 @@ async function fetchData() {
     const res = await fetch('https://sports-live-scores.p.rapidapi.com/football/live', options)
     const record = await res.json()
     
-    document.getElementById("scores").innerHTML = record.matches.filter(m => 
-        m['League ID'] == 1 || m['League ID'] == 33 || m['League ID'] == 36  || m['League ID'] == 4 ).map(item => {
+    record.matches = record.matches.filter(m => 
+        m['League ID'] == 1 || m['League ID'] == 33 || m['League ID'] == 36  || m['League ID'] == 4 );
+    
+    if (record.matches.length == 0) {
+        document.getElementById("scores").innerHTML = `<p>No matches in the top 5 leagues going on right now!<p>`
+    } else {
+    document.getElementById("scores").innerHTML = record.matches.map(item => {
         
         if (item['Home Score'] > item['Away Score']) {
-            return `<li><strong>${item['Home Team']}  ${item['Home Score']}</strong> - 
+            return `<li><strong id="win">${item['Home Team']}  ${item['Home Score']}</strong> - 
                 ${item['Away Team']}  ${item['Away Score']   } ~~~ <i>${item.Status}</i> \xa0\xa0 <u>${item.League}</u></li>`
         } else if (item['Home Score'] == item['Away Score']) {
             return `<li>${item['Home Team']}  ${item['Home Score']} - 
                 ${item['Away Team']}  ${item['Away Score']} ~~~ <i>${item.Status}</i>  \xa0\xa0 <u>${item.League}</u></li>`
         } else {
             return `<li>${item['Home Team']}  ${item['Home Score']} - 
-                <strong>${item['Away Team']}  ${item['Away Score']}</strong>  ~~~   <i>${item.Status}</i> \xa0\xa0 <u>${item.League}</u></li>`
+                <strong id="win">${item['Away Team']}  ${item['Away Score']}</strong>  ~~~   <i>${item.Status}</i> \xa0\xa0 <u>${item.League}</u></li>`
         }
         
         
     }).join(' ');
+    }
     console.log(record)
+
+    
+
 }
 fetchData();
