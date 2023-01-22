b1fc478c27771ee965234cda3c5ab4fa001f7f36
panavdua <114715893+panavdua@users.noreply.github.com>
2023-01-23 00:05:35 +0800

    Add Bundesliga and ISL leagues

--- script.js
+++ script.js
@@ -10,8 +10,11 @@ async function fetchData() {
     const res = await fetch('https://sports-live-scores.p.rapidapi.com/football/live', options)
     const record = await res.json()
     
+     
     record.matches = record.matches.filter(m => 
-        m['League ID'] == 1 || m['League ID'] == 33 || m['League ID'] == 36  || m['League ID'] == 4 );
+        m['League ID'] == 1 || m['League ID'] == 33 || m['League ID'] == 36  || m['League ID'] == 4 || m['League ID'] == 42
+        || m['League ID'] == 38566 );
+    
     
     if (record.matches.length == 0) {
         document.getElementById("scores").innerHTML = `<p>No matches in the top 5 leagues going on right now!<p>`
@@ -19,10 +22,10 @@ async function fetchData() {
     document.getElementById("scores").innerHTML = record.matches.map(item => {
         
         if (item['Home Score'] > item['Away Score']) {
-            return `<li><strong id="win">${item['Home Team']}  ${item['Home Score']}</strong> - 
+            return `<li>  <strong id="win">${item['Home Team']}  ${item['Home Score']}</strong> - 
                 ${item['Away Team']}  ${item['Away Score']   } ~~~ <i>${item.Status}</i> \xa0\xa0 <u>${item.League}</u></li>`
         } else if (item['Home Score'] == item['Away Score']) {
-            return `<li>${item['Home Team']}  ${item['Home Score']} - 
+            return `<li> ${item['Home Team']}  ${item['Home Score']} - 
                 ${item['Away Team']}  ${item['Away Score']} ~~~ <i>${item.Status}</i>  \xa0\xa0 <u>${item.League}</u></li>`
         } else {
             return `<li>${item['Home Team']}  ${item['Home Score']} - 
@@ -32,6 +35,7 @@ async function fetchData() {
         
     }).join(' ');
     }
+    console.log("hello")
     console.log(record)
 
     
